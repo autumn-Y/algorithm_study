aa57d76c02df28d8e65b67eee989bd010fa03641
autumn-Y <yellowpig0512@gmail.com>
2025-08-11 23:33:48 +0900

    0811

---
+++ level1.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+/* 2557 
+int main() {
+    cout << "Hello World!" << endl;
+    return 0;
+}
+*/
+
+/*1000
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << endl;
+    return 0;
+}
+*/
+
+/* 1001
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << a - b << endl;
+    return 0;
+}
+*/
+
+/*10998
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << a*b << endl;
+    return 0;
+}
+*/
+
+/*1008
+int main() {
+    double a, b;
+    cin >> a >> b;
+    cout << fixed; // 소수점 자리 고정
+    cout.precision(9); // 소수점 자리 지정
+    cout << a / b << endl;
+    return 0;
+}
+*/
