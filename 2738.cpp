3c6977d4f7b247083d7b51329af3277f51ad8059
autumn-Y <yellowpig0512@gmail.com>
2025-09-28 23:19:00 +0900

    0928

---
+++ 2738.cpp
@@ -0,0 +1,41 @@
+// 두 행렬의 합을 구하는 문제
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() { 
+    int n, m;
+
+    cin >> n >> m;
+
+    int a[n][m];
+    int b[n][m];
+    int result[n][m];
+
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<m; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<m; j++) {
+            cin >> b[i][j];
+        }
+    }
+    
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<m; j++) {
+            result[i][j] = a[i][j] + b[i][j];
+        }
+    }
+    
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<m; j++) {
+            cout << result[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
